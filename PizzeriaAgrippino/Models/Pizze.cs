3ba7ebf6003d56bc8a532c07fa47b653e69033c5
Agrippino <agrippinorussiello@outlook.it>
2022-05-11 13:28:55 +0200

    Aggiunti anche attributi validation più adattia  tutti quesi campi

--- PizzeriaAgrippino/Models/Pizze.cs
+++ PizzeriaAgrippino/Models/Pizze.cs
@@ -5,14 +5,18 @@ namespace PizzeriaAgrippino.Models
 {
     public class Pizze
     {
+        //inseriamo required che obbliga all'utente di inserire quel campo o una determinata informazione
         public int Id { get; set; }
         [Required(ErrorMessage="Il campo immagine è obbligatorio")]
         public string ImagePizza { get; set; }
         [Required(ErrorMessage = "Il campo nome è obbligatorio")]
+        [Url(ErrorMessage = "Non hai inserito un Url valido")]
         public string NamePizza { get; set; }
         [Required(ErrorMessage = "Il campo Descrizione è obbligatorio")]
+        [StringLength(10, ErrorMessage = "Hai superato il limire di caratteri massimo di 10")]
         public string DescriptionPizza { get; set; }
         [Required(ErrorMessage = "Il campo Prezzo è obbligatorio")]
+        [Range(0.01,30, ErrorMessage = "Mi dispaice hai inserito un prezzo non valido, i minimo è 0.01€ mentre il massimo è 30€ ")]
         public double PricePizza { get; set; }
         
         public Pizze()
